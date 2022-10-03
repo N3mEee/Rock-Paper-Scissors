9aecd61c33ea66533abf090a3a4754d8ff0e2bea
N3mEee <n3mebusiness@gmail.com>
2022-10-03 11:01:34 +0200

    Fix the getComputerChoice

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let items = ["rock", "paper", "scissors"]
-
+let computerChoice;
 
 function getComputerChoice(){
     let randomPick = Math.floor(Math.random()*3)
-    console.log(randomPick)
+    computerChoice = items[randomPick]
+    return computerChoice
 }
