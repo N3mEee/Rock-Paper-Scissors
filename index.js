cb733e46dd87bd26da4b2ae30e3837de979ddc72
N3mEee <n3mebusiness@gmail.com>
2022-10-03 13:34:40 +0200

    Add final logic for the game

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ let items = ["Rock", "Paper", "Scissors"];
 let computerChoice;
 let playerChoice;
 let result;
+let round = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 let getComputerChoice = () => {
     let randomPick = Math.floor(Math.random() * 3);
     computerChoice = items[randomPick];
-    console.info(`Computer: ${computerChoice}`)
+    // console.info(`Computer: ${computerChoice}`)
     return computerChoice;
 }
 
 let getPlayerChoice = () => {
     playerChoice = prompt("Pick an item:", "Rock, Paper, Scissors");
-    console.info(`Player: ${playerChoice}`)
+    // console.info(`Player: ${playerChoice}`)
     return playerChoice;
 }
-
 
 let playOneRound = (playerchoice, computerchoice) => {
 
@@ -26,9 +28,11 @@ let playOneRound = (playerchoice, computerchoice) => {
                 break;
             case items[1]:
                 result = "Computer Wins! Rock < Paper"
+                computerScore += 1;
                 break
             case items[2]:
                 result = "Player Wins! Rock > Scissors"
+                playerScore += 1
                 break
             default:
                 result = "Something went wrong!"
@@ -39,12 +43,14 @@ let playOneRound = (playerchoice, computerchoice) => {
         switch (computerchoice) {
             case items[0]:
                 result = "Player Wins! Paper > Rock"
+                playerScore += 1
                 break;
             case items[1]:
                 result = "Draw! Paper = Paper"
                 break
             case items[2]:
                 result = "Computer Wins! Paper < Scissors"
+                computerScore += 1;
                 break
             default:
                 result = "Something went wrong!"
@@ -55,9 +61,11 @@ let playOneRound = (playerchoice, computerchoice) => {
         switch (computerchoice) {
             case items[0]:
                 result = "Computer Wins! Scissors < Rock"
+                computerScore += 1;
                 break;
             case items[1]:
                 result = "Player Wins! Scissors > Paper"
+                playerScore += 1
                 break
             case items[2]:
                 result = "Draw! Scissors = Scissors"
@@ -73,10 +81,46 @@ let playOneRound = (playerchoice, computerchoice) => {
 }
 
 function game() {
-    playOneRound(getPlayerChoice(), getComputerChoice());
-    if (result === "Player input was fucking wrong or something else happend!"){
-        console.error(result);
-    }else{
-        console.log(result);
+    for (let i = 1; i <= 5; i++) {
+        playOneRound(getPlayerChoice(), getComputerChoice());
+
+        if (playerScore > computerScore && i >= 5) {
+            console.log(`Round: ${round = i}`);
+            console.log(result);
+            console.warn(`Player wins the game with: ${playerScore} : ${computerScore}`)
+            restartGame();
+            i = 0;
+        } else if (playerScore < computerScore && i >= 5) {
+            console.log(`Round: ${round = i}`);
+            console.log(result);
+            console.warn(`Computer wins the game with: ${computerScore} : ${playerScore}`)
+            restartGame();
+            i = 0;
+        } else if (playerScore === computerScore && i >= 5) {
+            console.log(`Round: ${round = i}`);
+            console.log(result);
+            console.warn(`It's a draw, final score: ${computerScore} : ${playerScore}`)
+            restartGame();
+            i = 0;
+        } else if (playerScore < 5 && computerScore < 5 && i < 5) {
+            if (result === "Player input was fucking wrong or something else happend!" || result === "Something went wrong!") {
+                console.log(`Round: ${round = i}`);
+                console.warn(result);
+                restartGame();
+                i = 0;
+            } else {
+                console.log(`Round: ${round = i}`);
+                console.log(result);
+                console.log(`Player: ${playerScore} / Computer: ${computerScore}`)
+            }
+        } else {
+            console.warn("Something is fucked up")
+        }
     }
+}
+
+function restartGame() {
+    console.warn(`Restart The game`);
+    playerScore = 0;
+    computerScore = 0;
 }
